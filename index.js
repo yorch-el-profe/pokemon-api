db1d63c5394ab92be384f6a910a629de994ee41f
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-10-11 20:42:22 -0500

    Integración de POKEAPI con Express

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+const API = "https://pokeapi.co/api/v2";
+
+app.get("/pokemon", async function (request, response) {
+  const { data } = await axios.get(`${API}/pokemon?limit=151`);
+
+  const responses = await Promise.all(
+    data.results.map((x) => axios.get(x.url))
+  );
+
+  const pokemon = responses
+    .map(({ data }) => data)
+    .map((p) => ({
+      number: p.id,
+      name: p.name,
+    }));
+
+  response.json({ data: pokemon });
+});
+
+app.get("/pokemon/:id", async function (request, response) {
+  const { id } = request.params;
+  const { data } = await axios.get(`${API}/pokemon/${id}`);
+  response.json({
+    number: data.id,
+    name: data.name,
+    types: data.types.map((t) => t.type.name),
+    artwork: data.sprites.other["official-artwork"].front_default,
+    sprite: data.sprites.front_default,
+  });
+});
+
+app.listen(process.env.PORT || 3000);
